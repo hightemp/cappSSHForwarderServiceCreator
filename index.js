4a76f2dc23329e0e4db62ff53b609929e64a5149
Panov Anton <hightemp.unknown@gmail.com>
2020-03-15 13:22:01 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 const fs = require('fs');
+const path = require('path');
 const Client = require('ssh2').Client;
 
 const $log = console.log.bind(console);
+const $err = console.error.bind(console);
+const $dir = console.dir.bind(console);
 
 var sConfigPath = "~/.cappSSHForwarderServiceCreator.config.json";
 
@@ -68,9 +71,11 @@ async function fnExec(oConnection, sCommand, sStreamWriteText='')
         stream
           .on('exit', function() {
             $log('Stream :: exit');
+            fnSuccess();
           })
           .on('end', function() {
             $log('Stream :: end');
+            fnSuccess();
           })
           .on('error', function(err) {
             $log('Stream :: error: ' + err);
@@ -81,8 +86,9 @@ async function fnExec(oConnection, sCommand, sStreamWriteText='')
             fnSuccess();
           })
           .on('data', (data) => {
-            $log('Stream :: stdout :: data: ' + data);
-            fnSuccess(data);
+            $log('Stream :: stdout :: data:');
+            $dir(data.toString());
+            // fnSuccess(data);
           })
           .stderr
           .on('data', (data) => {
@@ -122,7 +128,7 @@ After=network.target
 [Service]
 Environment="LOCAL_ADDR=localhost"
 EnvironmentFile=/etc/default/secure-tunnel@%i
-ExecStart=/usr/bin/ssh -vvv -NT -C -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -D \${LOCAL_ADDR}:\${LOCAL_PORT} \${TARGET}
+ExecStart=/usr/bin/ssh -vvv -f -F /root/.ssh/config -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -D \\\${LOCAL_ADDR}:\\\${LOCAL_PORT} -NT -C \\\${TARGET}
 
 # Restart every >2 seconds to avoid StartLimitInterval failure
 RestartSec=5
@@ -169,18 +175,54 @@ for (var sServerName in oConfig) {
         try {
           // var sSudo = `echo "${oConfigItem.sSudoPassword}" | sudo -S`;
 
+/**
+ * .ssh directory: 700 (drwx------)
+ * public key (.pub file): 644 (-rw-r--r--)
+ * private key (id_rsa): 600 (-rw-------)
+ * lastly your home directory should not be writeable by the group or others (at most 755 (drwxr-xr-x)).
+ */
+
+          if (!oConfigItem.sSSHConfigPath) {
+            throw `sSSHConfigPath not defined for '${sServerName}'`;
+          }
+          if (!oConfigItem.sSSHKeyPath) {
+            throw `sSSHKeyPath not defined for '${sServerName}'`;
+          }
+
+          var sConfigFileContent = fs.readFileSync(oConfigItem.sSSHConfigPath).toString();
+          var sConfigFileName = path.basename(oConfigItem.sSSHConfigPath);
+          var sKeyFileContent = fs.readFileSync(oConfigItem.sSSHKeyPath).toString();
+          var sKeyFileName = path.basename(oConfigItem.sSSHKeyPath);
+
+          if (!sConfigFileContent.trim()) {
+            throw `sConfigFileContent is empty'`;
+          }
+          if (!sKeyFileContent.trim()) {
+            throw `sKeyFileContent is empty`;
+          }
+
           await fnExec(oConnection, `sudo su`, 
 `${oConfigItem.sSudoPassword}
 whoami
-cat <<EOF > ${sUnitFileConfigPath} ${sUnitFileConfig}\nEOF
-cat <<EOF > ${sUnitFilePath} ${sUnitFileTemplate}\nEOF
+cat <<EOF > /root/.ssh/${sConfigFileName}\n${sConfigFileContent}\nEOF
+cat <<EOF > /root/.ssh/${sKeyFileName}\n${sKeyFileContent}\nEOF
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/${sKeyFileName}
+cat <<EOF > ${sUnitFileConfigPath}\n${sUnitFileConfig}\nEOF
+cat <<EOF > ${sUnitFilePath}\n${sUnitFileTemplate}\nEOF
 systemctl daemon-reload
-systemctl start secure-tunnel@${sServiceName}
+systemctl restart secure-tunnel@${sServiceName}
 systemctl enable secure-tunnel@${sServiceName}
 exit
 `);
           
           $log("************ EXIT ************");
+
+          $log(`CHECK status:`);
+          $log(`1. sudo systemctl status secure-tunnel@${sServiceName}`);
+          $log(`2. sudo journalctl -u secure-tunnel@${sServiceName}`);
+
+          $log("******************************");
 
           oConnection.end();
         } catch (oError) {
